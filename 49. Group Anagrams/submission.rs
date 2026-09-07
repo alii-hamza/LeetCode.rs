b1be94994536d7e768f74bc92b75cd2a014ce872
𝗔𝗹𝗶 𝗛𝗮𝗺𝘇𝗮 <ali.javed.hamza@gmail.com>
2026-09-07 11:07:07 +0500

    Implement character counting approach for anagrams

--- 49. Group Anagrams/submission.rs
+++ 49. Group Anagrams/submission.rs
@@ -1,3 +1,7 @@
+# ==========================================
+# APPROACH 1: Sorting
+# ==========================================
+
 use std::collections::HashMap;
 impl Solution {
     pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
@@ -10,4 +14,32 @@ impl Solution {
         }
         map.into_values().collect()
     }
+}
+
+
+
+# ==========================================
+# APPROACH 2: Counting (Fast/Optimal)
+# ==========================================
+
+use std::collections::HashMap;
+impl Solution {
+  pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
+    let mut map: HashMap<[u8;26], Vec<String>> = HashMap::with_capacity(strs.len());
+    let offset = 'a' as usize;
+
+    for str in strs.into_iter() {
+      let mut chars: [u8; 26] = [0; 26];
+
+      for char in str.chars() {
+        chars[char.to_ascii_lowercase() as usize - offset] += 1;
+      }
+
+      map.entry(chars)
+        .and_modify(|v| v.push(str.clone()))
+        .or_insert(vec![str]);
+    }
+    
+    map.values().cloned().collect::<Vec<Vec<String>>>()
+  }
 }
