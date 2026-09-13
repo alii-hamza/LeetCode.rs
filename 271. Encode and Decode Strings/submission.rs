d10f5a8eae46018a4fef788e67eb555bb8c1ed3e
𝗔𝗹𝗶 𝗛𝗮𝗺𝘇𝗮 <ali.javed.hamza@gmail.com>
2026-09-13 12:39:50 +0500

    Implement Codec for encoding and decoding strings

---
+++ 271. Encode and Decode Strings/submission.rs
@@ -0,0 +1,52 @@
+/*
+You can run this code on:    play.rust-lang.org
+*/
+
+struct Codec;
+
+impl Codec {
+    fn new() -> Self {
+        Self
+    }
+
+    fn encode(&self, strs: Vec<String>) -> String {
+        let mut out = String::new();
+        for s in &strs {
+            out.push_str(&format!("{}#{}", s.len(), s));
+        }
+        out
+    }
+
+    fn decode(&self, s: String) -> Vec<String> {
+        let mut res = Vec::new();
+        let mut i = 0;
+        while i < s.len() {
+            let hash = s[i..].find('#').unwrap();
+            let j = i + hash;
+            let len: usize = s[i..j].parse().unwrap();
+            let start = j + 1;
+            let end = start + len;
+            res.push(s[start..end].to_string());
+            i = end;
+        }
+        res
+    }
+}
+
+fn main() {
+    let codec = Codec::new();
+
+    let tests = vec![
+        vec!["Hello".to_string(), "World".to_string()],
+        vec!["".to_string()],
+        vec!["#".to_string(), "a#b#c".to_string()],
+    ];
+
+    for input in tests {
+        let encoded = codec.encode(input.clone());
+        let decoded = codec.decode(encoded.clone());
+        println!("Input: {:?} -> Encoded: {:?} -> Decoded: {:?}", input, encoded, decoded);
+        assert_eq!(input, decoded);
+    }
+    println!("All passed!");
+}
