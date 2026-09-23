34f7e1baed6aac77da8dfe984fcf8db341a2424a
𝗔𝗹𝗶 𝗛𝗮𝗺𝘇𝗮 <ali.javed.hamza@gmail.com>
2026-09-23 10:49:56 +0500

    Implement max_area function for the problem

---
+++ 11. Container With Most Water/submission.rs
@@ -0,0 +1,21 @@
+impl Solution {
+    pub fn max_area(height: Vec<i32>) -> i32 {
+        let mut result = 0;
+        let mut iter = height.iter();
+        let mut p1 = iter.next();
+        let mut p2 = iter.next_back();
+        let mut width = height.len() - 1;
+
+        while let (Some(h1), Some(h2)) = (p1, p2){
+            result = result.max(h1.min(h2) * width as i32);
+            width -= 1;
+
+            if h1 < h2 {
+                p1 = iter.next();
+            } else {
+                p2 = iter.next_back();
+            }
+        }
+        result
+    }
+}
